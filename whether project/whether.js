28e923bb3f66cc167c32a6b27fb54e14ec838b4c
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-01-25 19:34:17 +0530

    Initial commit - React projects

--- whether project/whether.js
+++ whether project/whether.js
@@ -1,2 +1,25 @@
-search=document.querySelector(".js-search").value
-console.log(search);
+const BASE_URL="https://api.openweathermap.org/data/2.5";
+const API_KEY="bab11adb04e51cf78a2a79e2720b22a0";
+function searchHandel(){
+    let search=document.querySelector(".js-search").value
+    let city=search.trim();
+    if (city==""){
+        alert("Enter The valid city Name")
+    }
+    else{
+        fetch(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            console.log("city :",data.name);
+           
+            
+        })
+        .catch(err=>{console.log(err)})
+    }
+    
+    
+}
+
+
+
